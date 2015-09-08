22c5e44d25cef993e6366ddf612cbbfe78261bd9
alhric <alhriclacle@gmail.com>
2015-09-08 15:31:03 +0200

    kut

--- Duckhunt/Duckhunt/ViewModel/GameVM.cs
+++ Duckhunt/Duckhunt/ViewModel/GameVM.cs
@@ -2,11 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Duckhunt.ViewModel
 {
     class GameVM
     {
+        Boolean playable = true;
+        
+        public void Play()
+        {
+            Initialise();
+            LoadGraphicsContent();
+            while (playable)
+            {
+                Update();
+
+                try
+                {
+                    Thread.Sleep(20);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+	          
+        }
+
+        private void Initialise()
+        {
+
+        }
+        private void Update()
+        {
+
+        }
+        private void LoadGraphicsContent()
+        {
+
+        }
+
     }
 }
